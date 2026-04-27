d523f76a19c828cf80e119da60af51c7ce545f19
Selest <shcherbina.marusya@gmail.com>
2026-04-27 19:05:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 import random
+
+
+db = SQLAlchemy()
+
+class Question(db.Model):
+    __tablename__ = 'questions'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    answers = db.Column(db.JSON) 
+    correct_answer = db.Column(db.String(255), nullable=False)
+    question_type = db.Column(db.String(10), nullable=False)
+    city_name = db.Column(db.String(100))
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
