0b8b26a51c133071276459cf8ae86ca32a7b2235
julykrutilina-dev <july.krutilina@gmail.com>
2026-05-14 21:09:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,42 @@ IMAGES = {
     'marvel':     '965417/7aff91b41cff6aba01d6',
 }
 
+SOUNDS = {
+    'correct': '<speaker audio="alice-sounds-animals-cat-1.opus">',  # Встроенные звуки Alice
+    'wrong':   '<speaker audio="alice-sounds-animals-cat-2.opus">',
+    'intro':   '<speaker audio="alice-music-game-1.opus">',
+}
+
+def make_response(text, tts=None, end_session=False, buttons=None,
+                  image_id=None, image_title=None, image_desc=None,
+                  card_type=None, map_url=None):
+
+    response = {
+        'text': text,
+        'tts': tts or text,
+        'end_session': end_session,
+    }
+
+    if buttons:
+        response['buttons'] = [
+            {'title': btn, 'hide': True} for btn in buttons
+        ]
+
+
+    if image_id:
+        response['card'] = {
+            'type': 'BigImage',
+            'image_id': image_id,
+            'title': image_title or text[:50],
+            'description': image_desc or '',
+        }
+
+    return {'response': response, 'version': '1.0'}
+
+def make_image_gallery(items):
+    return {
+        'type': 'ItemsList',
+        'header': {'text': 'Посмотри на карте:'},
+        'items': items,
+        'footer': {'text': 'Продолжаем игру!'}
+    }
