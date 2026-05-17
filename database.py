8519651abb079bc822f9df7711778d1507a080c3
julykrutilina-dev <july.krutilina@gmail.com>
2026-05-17 22:29:24 +0300

    Update database.py
    Signed-off-by: julykrutilina-dev <july.krutilina@gmail.com>

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
-def init_db():
-    db.create_all()
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
