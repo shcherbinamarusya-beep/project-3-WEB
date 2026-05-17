4356a64a4260814130a587de058b35bd1623b1ed
julykrutilina-dev <july.krutilina@gmail.com>
2026-05-18 00:21:42 +0300

    Update maps.py
    Signed-off-by: julykrutilina-dev <july.krutilina@gmail.com>

--- maps.py
+++ maps.py
@@ -4,6 +4,7 @@ from typing import Optional
 logger = logging.getLogger(__name__)
 
 YANDEX_STATIC_MAPS_URL = 'https://static-maps.yandex.ru/1.x/'
+YANDEX_MAPS_URL = 'https://yandex.ru/maps/'
 
 
 def get_static_map_url(
@@ -28,4 +29,24 @@ def get_static_map_url(
         return url
     except Exception as e:
         logger.error(f'Ошибка при формировании URL карты: {e}')
+        return None
+
+
+def get_yandex_maps_url(
+    latitude: float,
+    longitude: float,
+    city_name: str = '',
+    zoom: int = 10,
+) -> Optional[str]:
+    try:
+        url = (
+            f'{YANDEX_MAPS_URL}'
+            f'?ll={longitude}%2C{latitude}'
+            f'&z={zoom}'
+            f'&pt={longitude}%2C{latitude}%2Cpm2rdm'
+        )
+        logger.info(f'Место на карте для «{city_name}»: {url}')
+        return url
+    except Exception as e:
+        logger.error(f'Ошибка при формировании URL Яндекс.Карт: {e}')
         return None
