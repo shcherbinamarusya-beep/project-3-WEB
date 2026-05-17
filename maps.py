015b5b26da9a1510008e0fc0af054da1529dfb39
julykrutilina-dev <july.krutilina@gmail.com>
2026-05-17 22:13:53 +0300

    Create maps.py
    Signed-off-by: julykrutilina-dev <july.krutilina@gmail.com>

---
+++ maps.py
@@ -0,0 +1,29 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+YANDEX_STATIC_MAPS_URL = 'https://static-maps.yandex.ru/1.x/'
+
+
+def get_static_map_url(
+    latitude: float,
+    longitude: float,
+    city_name: str = '',
+    zoom: int = 10,
+    size: str = '450,450',
+) -> str | None:
+
+    try:
+        pt = f'{longitude},{latitude},pm2rdm'
+        url = (
+            f'{YANDEX_STATIC_MAPS_URL}'
+            f'?ll={longitude},{latitude}'
+            f'&z={zoom}'
+            f'&size={size}'
+            f'&pt={pt}'
+            f'&l=map'
+        )
+        logger.info(f'Карта для «{city_name}»: {url}')
+        return url
+    except Exception as e:
+        logger.error(f'Ош
